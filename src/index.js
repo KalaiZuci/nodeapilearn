e2840c96b1bc414a7c62e4a6293655dac96e35d3
Kalaiarasi Pandiarajan <kalaiarasi.rajan@zucisystems.com>
2022-07-05 13:26:36 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
-//get call post call
-dotenv.config();
 const port = process.env.PORT || 8080;
+dotenv.config();
 
 //middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(require('./router.js'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("./router.js"));
 
-app.listen(port, ()=>console.log('APP Listening Port ' + port));
+app.listen(port, () => console.log("APP Listening Port " + port));
